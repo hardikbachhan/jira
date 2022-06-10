ddccda04cb5153027391209cbf89e5ab2e37e607
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-06-10 23:43:56 +0530

    class 2 of Jira

---
+++ script.js
@@ -0,0 +1,38 @@
+let addBtn = document.querySelector(".add-btn");
+// console.log(addBtn);
+
+var isModalPresent = false;
+
+addBtn.addEventListener("click", function(){
+
+    // case 1 -> if screen is empty / modal is not present
+    if (!isModalPresent) {
+        // then display modal
+    } 
+
+    // case 2 -> if modal is present
+    else {
+        // then hide modal
+    }
+
+    isModalPresent = !isModalPresent;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
